c94397b40c5d23da40def87985297cd1da5f9797
dejisol <hellodejisol@gmail.com>
2021-09-12 09:49:10 +0100

    Update package

--- udagram-api-user/src/server.ts
+++ udagram-api-user/src/server.ts
@@ -6,12 +6,12 @@ import {IndexRouter} from './controllers/v0/index.router';
 
 import bodyParser from 'body-parser';
 import {config} from './config/config';
-import {V0_USER_MODELS} from './controllers/v0/model.index';
+import {V0MODELS} from './controllers/v0/model.index';
 
 const c = config;
 
 (async () => {
-  await sequelize.addModels(V0_USER_MODELS); 
+  await sequelize.addModels(V0MODELS);
   await sequelize.sync();
 
   const app = express();
@@ -19,31 +19,23 @@ const c = config;
 
   app.use(bodyParser.json());
 
-  // We set the CORS origin to * so that we don't need to
-  // worry about the complexities of CORS this lesson. It's
-  // something that will be covered in the next course.
   app.use(cors({
-    allowedHeaders: [
-      'Origin', 'X-Requested-With',
-      'Content-Type', 'Accept',
-      'X-Access-Token', 'Authorization',
-    ],
-    methods: 'GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE',
-    preflightContinue: true,
-    origin: '*',
+    allowedHeaders: ["Origin", "X-Requested-With", "Content-Type", "Accept", "X-Access-Token", "Authorization"],
+    methods: "GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE",
+    origin: "*"
   }));
 
-  app.use('/api/v0/', IndexRouter);
+  app.use('/api/v0/', IndexRouter)
 
   // Root URI call
-  app.get( '/', async ( req, res ) => {
-    res.send( '/api/v0/' );
+  app.get( "/", async ( req, res ) => {
+    res.send( "/api/v0/" );
   } );
 
 
   // Start the Server
   app.listen( port, () => {
-    console.log( `server running ${config.url}` );
-    console.log( `press CTRL+C to stop server` );
+      console.log( `server running ` + c.url );
+      console.log( `press CTRL+C to stop server` );
   } );
 })();
